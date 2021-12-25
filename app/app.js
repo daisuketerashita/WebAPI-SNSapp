4674e247a8a9935dfdfecec527a9a697b95a4762
daisuketerashita <ekusiad.tera@icloud.com>
2021-12-25 21:12:13 +0900

    データベース接続

--- app/app.js
+++ app/app.js
@@ -1,8 +1,38 @@
 const express = require('express')
 const app = express()
+const sqlite3 = require('sqlite3')
+const dbPath = "app/db/database.sqlite3"
 
-app.get('/api/v1/hello',(req,res) => {
-    res.json({"message": "Hello,world!"})
+app.get('/api/v1/users',(req,res) => {
+    const db = new sqlite3.Database(dbPath)
+
+    db.all('SELECT * FROM users',(err,rows) => {
+        res.json(rows)
+    })
+
+    db.close()
+})
+
+app.get('/api/v1/users/:id',(req,res) => {
+    const db = new sqlite3.Database(dbPath)
+    const id = req.params.id
+
+    db.get(`SELECT * FROM users WHERE id = ${id}`,(err,row) => {
+        res.json(row)
+    })
+
+    db.close()
+})
+
+app.get('/api/v1/search',(req,res) => {
+    const db = new sqlite3.Database(dbPath)
+    const keyword = req.query.q
+
+    db.all(`SELECT * FROM users WHERE name LIKE "%${keyword}%"`,(err,rows) => {
+        res.json(rows)
+    })
+
+    db.close()
 })
 
 const port = process.env.PORT || 3000;
